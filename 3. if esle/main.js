cd576e674974739993be381442043f6c358ac603
cocodemus <54139095+cocodemus@users.noreply.github.com>
2019-08-21 12:29:02 +0700

    Create main.js

---
+++ 3. if esle/main.js
@@ -0,0 +1,33 @@
+var nama = 'Joni'
+var peran = 'Pendekar Perkasa'
+
+if ((nama == false) && (peran == false)){
+    console.log('Masukin namanya bang!')
+}
+
+var nama = "Mikael"
+var peran = ''
+if ((nama == true) || (peran == false)) {
+    console.log('Halo', nama, 'Pilih peranmu untuk memulai game!')
+}
+
+var nama = 'Nina'
+var peran = 'Ksatria'
+if ((nama !== true) && (peran !== true)) {
+    console.log('Selamat datang di Dunia Proxytia,', nama)
+    console.log('Halo', peran, nama, 'kamu dapat menyerang dengan senjatamu!')
+}
+
+var nama = 'Danu'
+var peran = 'Thabib'
+if ((nama !== true) && (peran !== true)) {
+    console.log('Selamat datang di Dunia Proxytia,', nama)
+    console.log('Halo', peran, nama, 'kamu akan membantu temanmu yang terluka.')
+}
+
+var nama = 'Zero'
+var peran = 'Penyihir'
+if ((nama !== true) || (peran == true)) {
+    console.log('Selamat datang di Dunia Proxytia,', nama)
+    console.log('Halo', peran, nama, 'ciptakan keajaiban yang membantu kemenanganmu!')
+}
