792ec961e27abe4cd3fc8ff306da06d7d7c39a0e
cocodemus <54139095+cocodemus@users.noreply.github.com>
2019-08-21 23:28:59 +0700

    Create looping for.js

---
+++ 6. looping/looping for.js
@@ -0,0 +1,9 @@
+console.log('LOOPING PERTAMA')
+for (angka = 1; angka < 21; angka++) {
+    console.log(angka + ' - I love coding')
+}
+
+console.log('LOOPING KEDUA')
+for (angka = 20; angka > 0; angka--) {
+    console.log(angka + ' - I will become fullstack developer')
+}
