e2041c252d08cc9d745f8810db61511f5c281eb2
cocodemus <54139095+cocodemus@users.noreply.github.com>
2019-08-21 23:27:49 +0700

    Add files via upload

---
+++ 5. string/4string.js
@@ -0,0 +1,19 @@
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word4.substring(0, 3);
+var secondWord4 = word4.substring(4, 14); // do your own!
+var thirdWord4 = word4.substring(15, 17); // do your own!
+var fourthWord4 = word4.substring(18, 20); // do your own!
+var fifthWord4 = word4.substring(21, 25); // do your own!
+
+var firstWordLength4 = exampleFirstWord4.length;
+var lengthsecondWord4 = secondWord4.length;
+var lengththirdWord4 = thirdWord4.length;
+var lengthfourthWord4 = fourthWord4.length;
+var lengthfifthWord4 = fifthWord4.length;
+// create new variables around here
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength4);
+console.log('Second Word: ' + secondWord4 + ', with length: ' + lengthsecondWord4);
+console.log('Third Word: ' + thirdWord4 + ', with length: ' + lengththirdWord4);
+console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + lengthfourthWord4);
+console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + lengthfifthWord4);
